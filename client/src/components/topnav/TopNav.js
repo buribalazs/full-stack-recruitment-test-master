a0f338d86af7ed0391f6c5b6907b33492610bcbe
buribalazs <buribalazs@gmail.com>
2017-08-23 09:58:14 +0200

    add bpk-component switch logo to match design add menu icon

--- client/src/components/topnav/TopNav.js
+++ client/src/components/topnav/TopNav.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './TopNav.scss';
-import logo from '../..//logo.svg';
+import logo from '../../logo.svg';
+import BpkLargeMenuIcon from 'bpk-component-icon/lg/menu';
 
 const TopNav = () => (
-  <header className='header'>
-    <a href="/">
-      <span className='logoText'>Skyscanner</span>
-      <img className='logo' alt="Skyscanner" src={logo}/>
-    </a>
-     // TODO burger menu icon
-  </header>
+    <header className='header'>
+        <a href="/">
+            <span className='logoText'>Skyscanner</span>
+            <img className='logo' alt="Skyscanner" src={logo}/>
+        </a>
+        <a href="/">
+            <BpkLargeMenuIcon className='menuButton'/>
+        </a>
+    </header>
 );
 
 export default TopNav;
